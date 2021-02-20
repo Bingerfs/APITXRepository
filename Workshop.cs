523ceb89d645994ea481ea7d0a76130473950a32
Hector Hidalgo <marce.753@gmail.com>
2021-02-19 20:08:23 -0400

    Create Workshop class

---
+++ Workshop.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace apiExercise
+{
+    public class Workshop
+    {
+        public int Id { get; set; }
+
+        public string Status { get; set; }
+
+        public string Name { get; set; }
+
+    }
+}
